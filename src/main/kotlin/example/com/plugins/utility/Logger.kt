79f45b725c0c764162894a487512199bce1cf3aa
Soonto <soonto@p549248.kvmvps>
2024-09-15 01:20:48 +0300

    add combine logger

--- src/main/kotlin/example/com/plugins/utility/Logger.kt
+++ src/main/kotlin/example/com/plugins/utility/Logger.kt
@@ -26,6 +26,21 @@ private class FileLogger : Logger {
     }
 }
 
+
+private class CombineLogger: Logger{
+    val fileLogger = FileLogger()
+    val consoleLogger = CombineLogger()
+    override fun log(message: String) {
+        fileLogger.log(message)
+        consoleLogger.log(message)
+    }
+    override fun log(message: Exception) {
+        fileLogger.log(message)
+        consoleLogger.log(message)
+    }
+}
+
 val AppLogger: Logger = ConsoleLogger()
+
 
 fun printlnRED(message: String) = println("\u001B[31m$message\u001B[0m")
